e4419d30cfd716b3e15c8a4bd284d638fa5e4f42
Griffin1610 <PollyGS14@uww.edu>
2025-09-26 22:35:11 -0500

    fixed card-stat display issue

--- app/components/CardStats.tsx
+++ app/components/CardStats.tsx
@@ -2,17 +2,19 @@ import { Card } from "@/app/types/card";
 
 export default function CardStats({ cardData } : { cardData: Card }) {
 
-return(
-    <>
-        {cardData && (
-        <div>
-            <>
-                <h3>Name: {cardData.name}</h3>
-                <h3>CMC: {cardData.cmc}</h3>
-                <h3>Color: {cardData.colors[0]}</h3>
-            </>
-        </div>
-        )}
-    </>
-    )
+  if (!cardData) {
+    return <p>enter a card above to get started</p>;
+  }
+
+    return (
+        <> 
+            <div>
+                <>
+                    <h3>Name: {cardData.name ?? "N/A"}</h3>
+                    <h3>CMC: {cardData.cmc ?? "N/A"}</h3>
+                    <h3>Color: {cardData.colors?.[0] ?? "N/A"}</h3>
+                </>
+            </div>
+        </>
+        )
 }
